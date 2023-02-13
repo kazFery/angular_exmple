7c24c27764f9c683b48fa366e7f5583d1f564c4a
Farzaneh Kazemi <kazem.farzan@gmail.com>
2023-02-13 15:14:11 -0500

    add bootstrap and change app.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,18 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'bill-product';
+  quantity: number;
+  cost: number;
+  products: object;
+
+  constructor() {
+    this.quantity = 1;
+    this.cost = 0;
+    this.products = [];
+  }
 }
+git
